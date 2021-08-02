9cc72e03fa5b50141359fe0dfaf3305a84d2f7dd
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-02 14:31:28 -0400

    Create custom render method

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,13 @@
 class Skill < ActiveRecord::Base
+
+    def self.render_all
+        Skill.all.map do |skill|
+            {
+                id: skill.id,
+                name: skill.name,
+                image: skill.image
+            }
+        end
+    end
+
 end
