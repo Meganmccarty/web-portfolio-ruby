9269efde4cc633be2bbc99642852581484e0058e
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-02 15:24:20 -0400

    Create custom method

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,14 @@
 class Message < ActiveRecord::Base
+
+    def self.render_all
+        Message.all.map do |message|
+            {
+                id: message.id,
+                name: message.name,
+                email: message.email,
+                text: message.text
+            }
+        end
+    end
+
 end
